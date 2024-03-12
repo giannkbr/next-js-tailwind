052bb3d90d2de456aa1125bd1e0d1a1d6b6d51bd
giankbr <virgian.akbar@wingscorp.com>
2024-03-12 09:52:29 +0700

    feat : add tslint

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -42,7 +42,7 @@ const AriclePage = async () => {
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
-          {articles.map((article) => (
+          {articles.map((article : any) => (
             <Article key={article.slug} article={article} />
           ))}
         </div>
